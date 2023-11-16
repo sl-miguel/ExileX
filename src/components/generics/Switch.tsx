5c669d9ed71c5a08a6d9ae0e20f9b674e4c91cf1
Miguel S <62101239+sl-miguel@users.noreply.github.com>
2023-11-16 11:23:38 +0100

    🔁 Removed random margin

--- src/components/generics/Switch.tsx
+++ src/components/generics/Switch.tsx
@@ -6,7 +6,7 @@ function Switch() {
 	const handleToggle = () => setIsChecked(!isChecked);
 
 	return (
-		<label className='flex items-center space-x-2'>
+		<label className='flex items-center'>
 			<input type='checkbox' onChange={handleToggle} defaultChecked={isChecked} className='hidden' />
 			<span className={`w-10 h-5 ${isChecked ? 'bg-black' : 'bg-gray'} rounded-full p-[2px] flex items-center duration-300 transition-colors relative`}>
 				<span className={`block w-4 h-4 bg-white rounded-full shadow-md transform duration-300 transition-transform ${isChecked ? 'translate-x-5' : ''}`} />
