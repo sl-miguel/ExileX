5083454f6238c08f8f94de80c595de07251adda3
Miguel S <62101239+sl-miguel@users.noreply.github.com>
2023-11-17 12:15:43 +0100

    ♻️ Stylized loader

--- src/pages/Loader.tsx
+++ src/pages/Loader.tsx
@@ -1,7 +1,8 @@
 function Loader() {
 	return (
-		<div>
-			<h1>Loader</h1>
+		<div className='flex flex-col items-center justify-center gap-6 h-screen'>
+			<div className='w-36 h-36 animate-spin rounded-full inline-block border-solid border-t-4 border-t-black border-r-4 border-r-white'></div>
+			<h3 className='font-medium text-3xl'>League Client</h3>
 		</div>
 	);
 }
