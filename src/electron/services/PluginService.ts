05a9b0e6489d4ceea93dd6e3b7c054e83596dfb4
Gredon <sl.miguel.acc@gmail.com>
2023-11-28 18:33:48 +0100

    ✨ Added plugins loader

---
+++ src/electron/services/PluginService.ts
@@ -0,0 +1,41 @@
+import path from 'path';
+import fs from 'fs';
+
+class PluginService {
+  plugins: any[];
+
+  constructor() {
+    this.plugins = [];
+  }
+
+  getPlugins() {
+    return this.plugins;
+  }
+
+  async load() {
+    const basePath = path.join(__dirname, '../plugins');
+    const pluginsFiles = fs.readdirSync(basePath);
+    let totalPlugins = 0;
+
+    for (const pluginPath of pluginsFiles) {
+      const path = `${basePath}\\${pluginPath}`;
+      const Plugin = await require(path);
+
+      const plugin = new Plugin();
+      const pluginSettings = plugin.setup();
+
+      const pluginObject = {
+        name: pluginPath,
+        settings: pluginSettings,
+        path,
+      };
+
+      this.plugins.push(pluginObject);
+      totalPlugins += 1;
+    }
+
+    console.log(`Loaded ${totalPlugins} plugins.`);
+  }
+}
+
+export default PluginService;
