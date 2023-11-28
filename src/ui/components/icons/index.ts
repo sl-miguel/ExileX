b839636f1bc538e0676770353dba1a2efad46936
Gredon <sl.miguel.acc@gmail.com>
2023-11-28 19:00:49 +0100

    ✨ Added icons library

---
+++ src/ui/components/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as Exile } from './Exile';
+export { default as Plugin } from './Plugin';
+export { default as Chaos } from './Chaos';
+export { default as Profile } from './Profile';
+export { default as Chevron } from './Chevron';
