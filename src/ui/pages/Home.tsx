b1b1a54a1c6905a10c4d1b7cb78b8215a1276ac6
Gredon <sl.miguel.acc@gmail.com>
2023-12-07 23:12:02 +0100

    🔄️ Updating home route

--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -1,7 +1,17 @@
+import useFetch from '../hooks/useFetch';
+import { Me } from '../../electron/typings/MeTypings';
+import Avatar from '../components/profile/Avatar';
+import Level from '../components/profile/Level';
+// import Champions from '../components/account/Champions';
+
 function Home() {
+  const me = useFetch<Me>('/lol-chat/v1/me');
+
   return (
     <div>
-      <h1>Home</h1>
+      {me && <Avatar infos={me} />}
+      {me && <Level level={me.lol.level} />}
+      {/* <Champions /> */}
     </div>
   );
 }
