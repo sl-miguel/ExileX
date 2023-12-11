91d8bc841e3272a430b09f8e75960c289761c2d4
Gredon <sl.miguel.acc@gmail.com>
2023-12-11 16:20:57 +0100

    ✨ Implemented blitz mode logic

--- plugins/Blitz.js
+++ plugins/Blitz.js
@@ -1,7 +1,7 @@
 class Blitz {
   constructor() {
-    this.name = 'Test Mode';
-    this.description = 'Plugin that can test things.';
+    this.name = 'Blitz Mode';
+    this.description = 'Swaps between Jungle and Lane positions, granting you the freedom to switch roles during the champions select phase.';
     this.active = false;
     this.endpoint = '/';
   }
@@ -11,8 +11,6 @@ class Blitz {
 
     const configuration = [
       { id: 'blitz.radio', type: 'radio', value: 'Jungle', options: ['Lane', 'Jungle'] },
-      { id: 'blitz.paragraph', type: 'paragraph', value: 'This a paragraph, it gives some details to the settings..' },
-      { id: 'blitz.checkbox', type: 'checkbox', value: ['A'], options: ['A', 'B', 'C'] },
       { id: 'blitz.button', type: 'button', text: 'Validate' },
     ];
 
@@ -24,15 +22,14 @@ class Blitz {
   }
 
   async onPress(getSetting, lcu, settingId) {
-    const button = getSetting('blitz.button');
-    const outline = getSetting('blitz.button.outline');
+    const radio = getSetting('blitz.radio');
 
-    if (button.id === settingId) {
-      console.log('Plugin Blitz.js pressed.', settingId);
+    if (radio.value === 'Jungle') {
+      await lcu.request({ method: 'PATCH', url: `/lol-champ-select/v1/session/my-selection`, body: { spell1Id: 4, spell2Id: 11 } });
     }
 
-    if (outline.id === settingId) {
-      console.log('Plugin Blitz.js pressed.', settingId);
+    if (radio.value === 'Lane') {
+      await lcu.request({ method: 'PATCH', url: `/lol-champ-select/v1/session/my-selection`, body: { spell1Id: 4, spell2Id: 7 } });
     }
   }
 }
